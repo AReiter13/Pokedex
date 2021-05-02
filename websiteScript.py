56d5d29895216df25589c4907cd741c35f91de2e
AReiter13 <69998038+AReiter13@users.noreply.github.com>
2021-05-01 21:36:38 -0700

    stuff

---
+++ websiteScript.py
@@ -0,0 +1,54 @@
+from flask import Flask, redirect, url_for, render_template
+from flask_ngrok import run_with_ngrok
+import requests, re, time
+import torch, torchvision
+from torch import nn, optim
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+from google.colab import files
+from PIL import Image 
+
+device = torch.device('cuda:0') 
+model = models.resnet50(pretrained=True)
+model.fc = nn.Linear(model.fc.in_features, 75)
+model.to(device)
+model.load_state_dict(torch.load('websitestuff/model'))
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+
+def makePrediction(image):
+  xform = transforms.Compose([transforms.Resize((224,224)), transforms.ToTensor(), torchvision.transforms.RandomHorizontalFlip(p=0.5)])
+  image = xform(image).to(device)
+  model.eval()
+  image = image.unsqueeze(0)
+  output = model(image)
+  _, pred = torch.max(output.detach(), 1)
+  return pred
+
+app = Flask(__name__)
+
+run_with_ngrok(app)
+
+@app.route("/")
+def home():
+    return render_template("Home.html")
+
+@app.route("/predict/")
+def predict():
+    pred = makePrediction(Image.open('websitestuff/Growlithe.png')).item()+1
+    if pred == 58:
+      return redirect(url_for("growlithe"))
+    else:
+      return redirect(url_for("notGrowlithe"))
+    
+
+@app.route("/growlithe/")
+def growlithe():
+    return render_template("Growlithe.html")
+
+@app.route("/notGrowlithe")
+def notGrowlithe():
+    return f"not Growlithe"
+
+if __name__ == "__main__": 
+    app.run()
