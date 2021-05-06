eda38de2137eec73df163042896fa7865aa7eeb8
AReiter13 <69998038+AReiter13@users.noreply.github.com>
2021-05-06 13:45:46 -0700

    added pokemon

--- websiteScript.py
+++ websiteScript.py
@@ -46,8 +46,45 @@ def home():
             return render_template("Home.html")
       if img and allowed_file(img.filename):
             pred = makePrediction(Image.open(img)).item()+1
-            if pred == 58:
-                return redirect(url_for("growlithe"))
+            #REDIRECTIONS
+            if pred == 38:
+                return redirect(url_for("ninetales"))
+            elif pred == 39:
+                return redirect(url_for("jigglypuff"))
+            elif pred == 40:
+                return redirect(url_for("wigglytuff"))
+            elif pred == 41:
+                return redirect(url_for("zubat"))
+            elif pred == 42:
+                return redirect(url_for("golbat"))
+            elif pred == 43:
+                return redirect(url_for("oddish"))
+            elif pred == 44:
+                return redirect(url_for("gloom"))
+            elif pred == 45:
+                return redirect(url_for("vileplume"))
+            elif pred == 46:
+                return redirect(url_for("paras"))
+            elif pred == 47:
+                return redirect(url_for("parasect"))
+            elif pred == 48:
+                return redirect(url_for("venonat"))                                                                                
+            elif pred == 49:
+                return redirect(url_for("venomoth"))
+            elif pred == 50:
+                return redirect(url_for("diglett"))
+            elif pred == 51:
+                return redirect(url_for("dugtrio"))
+            elif pred == 52:
+                return redirect(url_for("meowth"))
+            elif pred == 53:
+                return redirect(url_for("persian"))
+            elif pred == 54:
+                return redirect(url_for("psyduck"))
+            elif pred == 55:
+                return redirect(url_for("golduck"))
+            elif pred == 56:
+                return redirect(url_for("mankey"))    
             else:
                 return redirect(url_for("rattata"))
       else:
@@ -56,17 +93,86 @@ def home():
       return render_template("Home.html")
     
 
-@app.route("/growlithe/")
-def growlithe():
-    return render_template("Growlithe.html")
+@app.route("/ninetales/")
+def ninetales():
+    return render_template("Ninetales.html")
+
+@app.route("/jigglypuff/")
+def jigglypuff():
+    return render_template("Jigglypuff.html")
+
+@app.route("/wigglytuff/")
+def wigglytuff():
+    return render_template("Wigglytuff.html")
+
+@app.route("/zubat/")
+def zubat():
+    return render_template("Zubat.html")
+
+@app.route("/golbat/")
+def golbat():
+    return render_template("Golbat.html")
+
+@app.route("/oddish/")
+def oddish():
+    return render_template("Oddish.html")
+
+@app.route("/gloom/")
+def gloom():
+    return render_template("Gloom.html")    
+
+@app.route("/vileplume/")
+def vileplume():
+    return render_template("Vileplume.html")
+
+@app.route("/paras/")
+def paras():
+    return render_template("Paras.html")
+
+@app.route("/parasect/")
+def parasect():
+    return render_template("Parasect.html")
+
+@app.route("/venonat/")
+def venonat():
+    return render_template("Venonat.html")
+
+@app.route("/venomoth/")
+def venomoth():
+    return render_template("Venomoth.html")
+
+@app.route("/diglett/")
+def diglett():
+    return render_template("Diglett.html")
+
+@app.route("/dugtrio/")
+def dugtrio():
+    return render_template("Dugtrio.html")
+
+@app.route("/meowth/")
+def meowth():
+    return render_template("Meowth.html")
+
+@app.route("/persian/")
+def persian():
+    return render_template("Persian.html")
+
+@app.route("/psyduck/")
+def psyduck():
+    return render_template("Psyduck.html")
+
+@app.route("/golduck/")
+def golduck():
+    return render_template("Golduck.html")
+
+@app.route("/mankey/")
+def mankey():
+    return render_template("Mankey.html")
 
 @app.route("/rattata/")
 def rattata():
     return render_template("Rattata.html")
 
-@app.route("/ppp/")
-def ppp():
-    return render_template("pokemon.html")
 
 
 if __name__ == "__main__": 
